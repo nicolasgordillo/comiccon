50ca006c5f8a72012b3f9972b3433a4add00be52
Nicolas Gordillo <nicogordillo@gmail.com>
2017-10-12 15:36:37 -0300

    Hola Mundo con Spring

---
+++ src/main/java/com/boot/comiccon/App.java
@@ -0,0 +1,17 @@
+package com.boot.comiccon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Java Main Class
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
